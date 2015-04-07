8e6f6359397f820abea4e3e27233856e20b54933
tinkerthaler <tinkerthaler@gmail.com>
2015-04-07 16:30:34 +0200

    Move default nginx location

--- nginx.nix
+++ nginx.nix
@@ -4,6 +4,6 @@
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   services = {
     nginx.enable = true;
-    nginx.config = pkgs.lib.readFile /root/nginx.conf;
+    nginx.config = pkgs.lib.readFile /etc/nginx/nginx.conf;
   };
 }
